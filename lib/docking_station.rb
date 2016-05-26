0598a264d612c7a911ae27b889aa60247ea1e5b6
Jonathan <j_passe@live.fr>
2016-05-26 18:01:58 +0100

    initial commit

---
+++ lib/docking_station.rb
@@ -0,0 +1,38 @@
+require_relative 'bike'
+
+class DockingStation
+attr_reader :capacity
+
+DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @bikes = Array.new
+    @capacity = capacity
+  end
+
+  attr_reader :bike
+
+  def release_bike
+    raise 'No mo bikes' if empty?
+    @bikes.pop
+  end
+
+  def dock bike
+    raise "No mo slots" if full?
+    @bikes << @bike
+  end
+
+  def report_broken_bike
+  end
+
+private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,70 @@
+require 'docking_station'
+
+describe DockingStation do
+subject(:docking_station) {described_class.new}
+
+ it {should respond_to :release_bike}
+ it {should respond_to(:dock).with(1).argument}
+ it {should respond_to :bike}
+
+  describe '#release_bike' do
+
+    # it 'releases working bikes' do
+    #   bike = docking_station.release_bike
+    #   expect(bike).to be_working
+    # end
+
+    # it 'releases a bike' do
+    #   bike = Bike.new
+    #   docking_station.dock(bike)
+    #   expect(docking_station.release_bike).to eq bike
+    # end
+
+    it 'raises an error when there are no bikes available' do
+      expect { docking_station.release_bike }.to raise_error 'No mo bikes'
+    end
+
+  end
+
+  describe '#dock' do
+
+    it 'raises an error when there are no slots available' do
+      DockingStation::DEFAULT_CAPACITY.times {docking_station.dock(Bike.new)}
+      expect {docking_station.dock(Bike.new)}.to raise_error "No mo slots"
+    end
+
+    # it 'docks a bike' do
+    #   bike = Bike.new
+    #   expect(docking_station.dock(bike)).to eq bike
+    # end
+
+  # end
+
+  # describe '#bike' do
+  #   it 'returns docked bike' do
+  #     bike = Bike.new
+  #     subject.dock(bike)
+  #     expect(docking_station.bike).to eq bike
+   # end
+   it 'initializes with a default value of 20' do
+    expect(docking_station.capacity).to eq 20
+    end
+
+   it 'has a default capacity' do
+    expect(docking_station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+   end
+
+   it 'initilizes with the same default capacity as DEFAULT_CAPACITY' do
+    DockingStation::DEFAULT_CAPACITY = 15
+    docking_station2 = DockingStation.new
+    expect(docking_station2.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+   it 'allows a user to report a broken bike when returning it' do
+    expect(docking_station).to respond_to :report_broken_bike
+  end
+
+
+  end
+
+end
