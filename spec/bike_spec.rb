b20e0d7ec3a58a0d8b6fd1e58185b34f6746f587
Jonathan <j_passe@live.fr>
2016-05-27 12:42:00 +0100

    lllll

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,13 +3,13 @@ require 'bike'
 describe Bike  do
 <<<<<<< HEAD
 
-it { should respond_to :working?} 
+it { should respond_to :working?}
 =======
-"======================="
+
   it { should respond_to :working?}
 >>>>>>> 3147d1bbb5701ba5a0416a0c77d74dc26df2e322
 
 # it 'should recognize a broken bike' do
-# expect(subject.broken).to eq true 
+# expect(subject.broken).to eq true
 # end
 end
